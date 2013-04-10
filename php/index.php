b5083e2c95e29ba79841d1e54e53cc61e1a06984
xiaoli <xiaoli@dhcp-9-116.nay.redhat.com>
2013-04-10 16:30:37 +0800

    make minor changes

--- php/index.php
+++ php/index.php
@@ -122,7 +122,7 @@
     <div class="brand-text"><strong>Open</strong>Shift</div>
   </a>
   <h1>
-    Welcome to OpenShift
+    Welcome to OpenShift TESTPHP
   </h1>
   <p>
     Place your application here
